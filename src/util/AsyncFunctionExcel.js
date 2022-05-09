85796a9c9972f40af4d35f1e267856ec59990a1e
Тарасенко Никита Сергеевич <NSTarasenko@sberbank.ru>
2022-05-09 07:09:18 +0300

    excel final

--- src/util/AsyncFunctionExcel.js
+++ src/util/AsyncFunctionExcel.js
@@ -1,20 +1,22 @@
 import axios from "axios";
 import authHeader, {URL_download} from "./api";
+import FileSaver from 'file-saver'
 
 export async function downloadExcel(setLoading, id, handleAccess) {
     try {
-        // setLoading(true)
+        setLoading(true)
         const data = {
             "idStaff": localStorage.getItem('idStaff'),
             "idFilter": id,
         }
-        const file = await axios.post(URL_download, data, {headers: authHeader()})
-        // const link = document.createElement('a');
-        // link.href = `your_link.pdf`;
-        // document.body.appendChild(link);
-        // link.click();
-        // document.body.removeChild(link);
-        // setLoading(false)
+        const file = await axios.post(URL_download, data, {headers: authHeader(), responseType: 'arraybuffer'})
+        const fileName = file.headers['content-disposition'].split('filename=')[1];
+        const blob = new Blob(
+            [file.data],
+            {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'}
+        )
+        FileSaver.saveAs(blob, fileName)
+        setLoading(false)
     } catch (e) {
         handleAccess(403)
     }
